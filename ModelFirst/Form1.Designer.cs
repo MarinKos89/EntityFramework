7070bdc902b197abf6e0ecf7f9bc0ab9bd4fa469
MarinKos89 <m.kos89@gmail.com>
2018-02-27 22:11:29 +0100

    Adding Model First aproach

---
+++ ModelFirst/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Zadatak71
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbKomercijalist = new System.Windows.Forms.ComboBox();
+            this.cbRacun = new System.Windows.Forms.ComboBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbKomercijalist
+            // 
+            this.cbKomercijalist.FormattingEnabled = true;
+            this.cbKomercijalist.Location = new System.Drawing.Point(84, 45);
+            this.cbKomercijalist.Name = "cbKomercijalist";
+            this.cbKomercijalist.Size = new System.Drawing.Size(248, 21);
+            this.cbKomercijalist.TabIndex = 0;
+            this.cbKomercijalist.SelectedIndexChanged += new System.EventHandler(this.cbKomercijalist_SelectedIndexChanged);
+            // 
+            // cbRacun
+            // 
+            this.cbRacun.FormattingEnabled = true;
+            this.cbRacun.Location = new System.Drawing.Point(84, 138);
+            this.cbRacun.Name = "cbRacun";
+            this.cbRacun.Size = new System.Drawing.Size(248, 21);
+            this.cbRacun.TabIndex = 1;
+            this.cbRacun.SelectedIndexChanged += new System.EventHandler(this.cbRacun_SelectedIndexChanged);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(486, 45);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(267, 20);
+            this.txtIme.TabIndex = 2;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(486, 96);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(267, 20);
+            this.txtPrezime.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(486, 139);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(267, 20);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(486, 188);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(267, 20);
+            this.txtTelefon.TabIndex = 5;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(84, 332);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(248, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(486, 332);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(267, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(84, 188);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 623);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.cbRacun);
+            this.Controls.Add(this.cbKomercijalist);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbKomercijalist;
+        private System.Windows.Forms.ComboBox cbRacun;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ ModelFirst/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak71
+{
+    public partial class Form1 : Form
+    {
+        public AdventureWorksOBPEntities entities = new AdventureWorksOBPEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbKomercijalist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbKomercijalist.SelectedIndex != -1)
+            {
+                UcitajRacune();
+                ClearForm();
+            }
+        }
+        private void UcitajRacune()
+        {
+            cbRacun.Items.Clear();
+            Komercijalist komercijalist = (Komercijalist)cbKomercijalist.SelectedItem;
+            cbRacun.Items.AddRange(komercijalist.Racun.Take(10).ToArray());
+        }
+        private void cbRacun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbRacun.SelectedIndex != -1)
+            {
+                PrikaziKupca();
+                PrikaziKarticu();
+            }
+        }
+        private void PrikaziKarticu()
+        {
+            Racun racun = (Racun)cbRacun.SelectedItem;
+            lblInfo.Text = racun.KreditnaKartica.Tip;
+        }
+
+        private void PrikaziKupca()
+        {
+            Racun racun = (Racun)cbRacun.SelectedItem;
+            Kupac kupac = racun.Kupac;
+
+            txtIme.Text = kupac.Ime;
+            txtPrezime.Text = kupac.Prezime;
+            txtEmail.Text = kupac.Email;
+            txtTelefon.Text = kupac.Telefon;
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Racun racun = (Racun)cbRacun.SelectedItem;
+            Kupac kupac = racun.Kupac;
+
+            kupac.Ime = txtIme.Text;
+            kupac.Prezime = txtPrezime.Text;
+            kupac.Email = txtEmail.Text;
+            kupac.Telefon = txtTelefon.Text;
+            entities.SaveChanges();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            Racun racun = (Racun)cbRacun.SelectedItem;
+            ICollection<Stavka> stavka = racun.Stavka;
+            entities.Stavka.RemoveRange(stavka);
+            entities.Racun.Remove(racun);
+            entities.SaveChanges();
+            UcitajRacune();
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            txtIme.Text = "";
+            txtPrezime.Text = "";
+            txtTelefon.Text = "";
+            txtEmail.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            UcitajKomercijaliste();
+        }
+
+        private void UcitajKomercijaliste()
+        {
+            cbKomercijalist.Items.Clear();
+            cbKomercijalist.Items.AddRange(entities.Komercijalist.ToArray());
+        }
+    }
+}
